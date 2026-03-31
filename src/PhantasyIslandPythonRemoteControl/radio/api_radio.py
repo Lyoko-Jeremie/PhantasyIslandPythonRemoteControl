e0ccda50cdb26f2efd36a1e7bbfecf2edc4bb788
LyokoJeremie <lucheng989898@gmail.com>
2026-03-31 11:36:48 +0800

    RadioApi

--- src/PhantasyIslandPythonRemoteControl/radio/api_radio.py
+++ src/PhantasyIslandPythonRemoteControl/radio/api_radio.py
@@ -13,8 +13,25 @@ class RadioApi(ApiModule):
 
     def isSceneInit(self) -> typing.Optional[dict]:
         return self._send_and_wait_sync('radio.isSceneInit')
+
     def isRadioReachabilityCheckerInit(self) -> typing.Optional[dict]:
         return self._send_and_wait_sync('radio.isRadioReachabilityCheckerInit')
 
     # def checkReachability(self, objectId: str) -> typing.Optional[dict]:
     #     return self._send_and_wait_sync('radio.checkReachability', data={'objectId': objectId})
+
+    ## TODO
+
+    def getAllRadioMaterial(self) -> typing.Optional[dict]:
+        return self._send_and_wait_sync('radio.getAllRadioMaterial')
+
+    def localRadioMaterial(self) -> typing.Optional[dict]:
+        return self._send_and_wait_sync('radio.localRadioMaterial')
+
+    def getBuildingRadioMaterial(self) -> typing.Optional[dict]:
+        return self._send_and_wait_sync('radio.getBuildingRadioMaterial')
+
+    ## TODO
+
+    def listRadioLocalObjects(self) -> typing.Optional[dict]:
+        return self._send_and_wait_sync('radio.listRadioLocalObjects')

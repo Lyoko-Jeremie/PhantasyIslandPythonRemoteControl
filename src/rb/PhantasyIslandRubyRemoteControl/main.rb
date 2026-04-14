c7ac24f1331c35c109a8ad0ff86f18e53d1db182
LyokoJeremie <lucheng989898@gmail.com>
2026-04-14 17:44:43 +0800

    ruby

--- src/rb/PhantasyIslandRubyRemoteControl/main.rb
+++ src/rb/PhantasyIslandRubyRemoteControl/main.rb
@@ -6,6 +6,6 @@ require_relative 'ph0apy'
 # PhantasyIslandRubyRemoteControl
 module PhantasyIslandRubyRemoteControl
   def self.get_airplane_manager
-    ::get_airplane_manager
+    get_airplane_manager
   end
 end
